38d034be83e39cb6f4e23c959b2da6150b38be73
monteima <matthieu.monteiro@reseau.eseo.fr>
2020-12-07 11:04:44 +0100

    Code smells logger

--- src/main/java/com/config/JDBCConfiguration.java
+++ src/main/java/com/config/JDBCConfiguration.java
@@ -3,6 +3,8 @@ package com.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 
 public class JDBCConfiguration {
@@ -10,6 +12,8 @@ public class JDBCConfiguration {
 		public static Connection getConnection() throws ClassNotFoundException{		
 
 			Connection conn = null;
+			Logger logger = Logger.getLogger("logger");
+
 			
 			String bdd = "ville_de_france";
 	    	String url = "jdbc:mysql://localhost:3306/" + bdd;
@@ -21,13 +25,13 @@ public class JDBCConfiguration {
         	
     	    Class.forName("com.mysql.jdbc.Driver");
     	    conn = DriverManager.getConnection(url, user, passwd);
-    	    System.out.println("Connecté à la bdd");
+    		logger.log(Level.INFO, "Connecté à la bdd");
+
 
         	
 			} catch (SQLException e) {
 	    	    e.printStackTrace();
-//	    	    LOGGER.log("context", e);
-	    	    System.out.println("Erreur");
+	    		logger.log(Level.INFO, "Erreur");
 	    	    System.exit(0);
 			}
 			return conn;
--- src/main/java/com/controller/VilleController.java
+++ src/main/java/com/controller/VilleController.java
@@ -1,8 +1,9 @@
 package com.controller;
 
-
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,12 +22,13 @@ public class VilleController {
 
 	@Autowired
 	VilleBLO villeBLOService;
+	Logger logger = Logger.getLogger("logger");
 
 	@RequestMapping(value = "/ville/get", method = RequestMethod.GET)
 	@ResponseBody
 	public ArrayList<Ville> appelGet(@RequestParam(required = false, value = "codeCommune") String param) {
 
-		System.out.println("Appel Get");
+		logger.log(Level.INFO, "Appel Get");
 
 		return villeBLOService.getInfoVille(param);
 
@@ -36,7 +38,7 @@ public class VilleController {
 	@ResponseBody
 	public String appelDelete(@RequestParam(required = false, value = "codePostal") String codePostal) {
 
-		System.out.println("Appel Delete");
+		logger.log(Level.INFO, "Appel Delete");
 		villeBLOService.deleteInfoVille(codePostal);
 
 		return "Delete completed";
@@ -47,7 +49,7 @@ public class VilleController {
 	@ResponseBody
 	public String appelPost(@RequestBody() Ville ville) throws ClassNotFoundException, SQLException {
 
-		System.out.println("Appel Post");
+		logger.log(Level.INFO, "Appel Post");
 		villeBLOService.postInfoVille(ville);
 
 		return "Post completed";
@@ -58,7 +60,7 @@ public class VilleController {
 	@ResponseBody
 	public String appelPut(@RequestBody() Ville ville) throws ClassNotFoundException, SQLException {
 
-		System.out.println("Appel Put");
+		logger.log(Level.INFO, "Appel Put");
 		villeBLOService.putInfoVille(ville);
 
 		return "Update completed";
